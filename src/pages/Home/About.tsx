3cb4544e4fc06d18acba730277bec2050f81056f
Jiří Strnadel <Juzba88@gmail.com>
2025-03-05 19:46:43 +0100

    update

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -1,9 +1,25 @@
-import "./scss/About.scss"
+import './scss/About.scss';
 
 const About = () => {
-  return (
-    <section className="about">About</section>
-  )
-}
+    return (
+        <section className="about">
+            <h1>O mně</h1>
+            <p>
+                Ahoj, jmenuji se Jiří Strnadel a věnuji se vývoji webových aplikací se zaměřením na React a TypeScript. Programování mě
+                nadchlo natolik, že mu věnuji každý volný moment už více než rok a půl. Neustále se učím nové technologie a zdokonaluji své
+                dovednosti.
+            </p>
+            <p>
+                Mám zkušenosti s JavaScriptem, SCSS, HTML, CSS, Git a databázemi jako Firebase. Zároveň si rozšiřuji znalosti v C#, kde se
+                pohybuji na mírně pokročilé úrovni.
+            </p>
+            <p>
+                Předtím jsem pracoval jako elektro údržbář a programátor průmyslových robotů Kuka, Reis, Yaskawa a PLC Simatic. Tato práce
+                mě naučila analytickému myšlení a efektivnímu řešení problémů, což dnes využívám při vývoji aplikací.
+            </p>
+            <p>Hledám příležitost, kde bych mohl své dovednosti dále rozvíjet a přispět svým nadšením pro moderní frontendový vývoj.</p>
+        </section>
+    );
+};
 
-export default About
+export default About;
