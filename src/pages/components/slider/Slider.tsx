1da1c5dd84feaaef1e9bc95d85e2af1e58b1818a
Jiří Strnadel <Juzba88@gmail.com>
2025-03-05 23:45:36 +0100

    slider done

--- src/pages/components/slider/Slider.tsx
+++ src/pages/components/slider/Slider.tsx
@@ -1,37 +1,63 @@
-import { useEffect, useState } from 'react';
-import './Slider.scss';
-import StackIcon from 'tech-stack-icons';
+import { useEffect, useState } from "react";
+import "./Slider.scss";
+import StackIcon from "tech-stack-icons";
 
-const iconArrey: string[] = ['html5', 'css3', 'sass', 'reactjs', 'js', 'typescript', 'csharp'];
+const iconArrey: string[] = ["html5", "css3", "sass", "reactjs", "js", "typescript", "csharp"];
 
 const Slider = () => {
-    const [position, setPosition] = useState<number>(0);
+	const [position, setPosition] = useState<number>(0);
 
-    console.log(position);
+	console.log(position);
 
-    useEffect(() => {
-        let count: number = position;
-        const t1 = setInterval(() => {
-            if (++count > iconArrey.length - 1) count = 0;
-            setPosition(count);
-        }, 3000);
+	useEffect(() => {
+		let count: number = position;
+		const t1 = setInterval(() => {
+			if (++count > iconArrey.length - 1) count = 0;
+			setPosition(count);
+		}, 4000);
 
-        return () => clearInterval(t1);
-    }, [position]);
+		return () => clearInterval(t1);
+	}, [position]);
 
-    const classNameFunction = (index: number) => {
-        if (index === position) return 'icon active';
-        else if (index === position + 1 || position === iconArrey.length - 1 && index === 0) return 'icon start-pos';
-        return 'icon end-pos';
-    };
 
-    return (
-        <div className="slider">
-            {iconArrey.map((item: string, index: number) => {
-                return <StackIcon key={index} className={classNameFunction(index)} name={item} />;
-            })}
-        </div>
-    );
+	// tri ikony
+	// 	const classNameFunction = (index: number) => {
+	// 	if (index === position) return "icon pos3";
+	// 	else if (index === position - 1 || (position === 0 && index === iconArrey.length - 1)) return "icon pos4";
+	// 	return "icon pos2";
+	// };
+
+	// pet ikon
+	const classNameFunction = (index: number) => {
+		if (index === position) return "icon pos3";
+
+		else if (
+			index === position - 1 || 
+			(position === 0 && index === iconArrey.length - 1)
+		) 
+		return "icon pos2";
+
+		else if (
+			index === position - 2 ||
+			(position === 0 && index === iconArrey.length - 2) ||
+			(position === 1 && index === iconArrey.length - 1)
+		)
+			return "icon pos1";
+		else if (
+			index === position + 1 ||
+			 (position + 1 > iconArrey.length - 1 && index === 0)
+			) 
+			return "icon pos4";
+		return "icon pos5";
+	};
+
+	return (
+		<div className="slider">
+			{iconArrey.map((item: string, index: number) => {
+				return <StackIcon key={index} className={classNameFunction(index)} name={item} />;
+			})}
+		</div>
+	);
 };
 
 export default Slider;
