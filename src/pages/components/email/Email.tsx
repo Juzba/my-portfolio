2386a57188b768947f3e923cee2cc705aec5c603
Jiří Strnadel <Juzba88@gmail.com>
2025-03-06 21:45:32 +0100

    working on email

--- src/pages/components/email/Email.tsx
+++ src/pages/components/email/Email.tsx
@@ -1,15 +1,23 @@
 import './Email.scss';
 
+const defaultState: {name: string, email: string, tel: string, company: string, message: string} = {
+    name: '',
+    email: '',
+    tel: "",
+    company: '',
+    message: '',
+};
+
 const Email = () => {
     return (
-        <section className='email'>
+        <section className="email">
             <h3>Email</h3>
             <form action="">
-                <input type="text" id="input-name" />
-                <input type="email" id="input-email"/>
-                <input type="tel" id="input-tel"/>
-                <input type="text" id="input-company" />
-                <input type="text" id="input-message"/>
+                <input type="text" id="input-name" placeholder="Jméno *" />
+                <input type="email" id="input-email" placeholder="Email *" />
+                <input type="tel" id="input-tel" placeholder="Telefon" />
+                <input type="text" id="input-company" placeholder="Firma" />
+                <textarea maxLength={800} name="message" id=""></textarea>
                 <input type="submit" />
             </form>
         </section>
