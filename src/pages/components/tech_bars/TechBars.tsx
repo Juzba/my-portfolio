ced2ab8f0eed7caf7addb2a5113a20d7ed301468
Jiří Strnadel <Juzba88@gmail.com>
2025-03-20 01:45:21 +0100

    update responsivity

--- src/pages/components/tech_bars/TechBars.tsx
+++ src/pages/components/tech_bars/TechBars.tsx
@@ -1,38 +1,37 @@
-import { useEffect, useState } from "react";
-import "./TechBars.scss";
-import StackIcon from "tech-stack-icons";
+import { useEffect, useState } from 'react';
+import './TechBars.scss';
+import StackIcon from 'tech-stack-icons';
 
-const bars: string[] = ["reactjs", "sass", "typescript", "csharp"];
+const bars: string[] = ['reactjs', 'sass', 'typescript', 'csharp'];
 
 const TechBars = () => {
-	const [animation, setAnimation] = useState("");
-console.log(animation);
+    const [animation, setAnimation] = useState('');
 
-	useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) setAnimation("active");
-			});
-		});
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) setAnimation('active');
+            });
+        });
 
-		const bars = document.getElementById("bars");
-		if (bars) observer.observe(bars);
-	}, []);
+        const bars = document.getElementById('bars');
+        if (bars) observer.observe(bars);
+    }, []);
 
-	return (
-		<div className="bars" id={"bars"}>
-			{bars.map((item, index) => {
-				return (
-					<div key={index} className="box">
-						<div className="circle">
-							<StackIcon className="icon" name={item} />
-						</div>
-						<div className={`bar ${item} ${animation}`}></div>
-					</div>
-				);
-			})}
-		</div>
-	);
+    return (
+        <div className="bars" id={'bars'}>
+            {bars.map((item, index) => {
+                return (
+                    <div key={index} className="box">
+                        <div className="circle">
+                            <StackIcon className="icon" name={item} />
+                        </div>
+                        <div className={`bar ${item} ${animation}`}></div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default TechBars;
