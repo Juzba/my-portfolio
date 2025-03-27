5beab0ffd72a118b7f51f12f8842a122e4cd065a
Jiří Strnadel <Juzba88@gmail.com>
2025-03-27 21:40:23 +0100

    update animation home

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -1,104 +1,140 @@
-import "./About-First-Box.scss";
-import "./About-Second-Box.scss";
-import "./About-Third-Box.scss";
-import "./About-Responsivity-Width.scss"
-import TechBars from "../components/tech_bars/TechBars";
-import Slider from "../components/slider/Slider";
-import imgBaldMan from "../../images/bman.png";
-import robot from "../../images/kuka.png";
-import comp from "../../images/comp.png";
-import Email from "../components/email/Email";
-import StackIcon from "tech-stack-icons";
-import { useEffect, useState } from "react";
+import './About-First-Box.scss';
+import './About-Second-Box.scss';
+import './About-Third-Box.scss';
+import './About-Responsivity-Width.scss';
+import TechBars from '../components/tech_bars/TechBars';
+import Slider from '../components/slider/Slider';
+import imgBaldMan from '../../images/bman.png';
+import robot from '../../images/kuka.png';
+import comp from '../../images/comp.png';
+import Email from '../components/email/Email';
+import StackIcon from 'tech-stack-icons';
+import { useEffect, useState } from 'react';
+
 
 const About = () => {
-	const [animationText1, setAnimationText1] = useState(false);
-	const [animationImg1, setAnimationImg1] = useState(false);
+    const [animation, setAnimation] = useState({
+        text0: false,
+        text1: false,
+        text2: false,
+        text3: false,
+        text4: false,
+        text5: false,
+        text6: false,
+    });
 
-	useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				// console.log(entry.target.id)
-				if (entry.isIntersecting && entry.target.id === "text1") setAnimationText1(true);
-				else if (entry.isIntersecting && entry.target.id === "img1") setAnimationImg1(true);
-			});
-		});
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+				
+                if (entry.isIntersecting && entry.target.id === 'text0') setAnimation({ ...animation, text0: true });
+                else if (entry.isIntersecting && entry.target.id === 'text1') setAnimation({ ...animation, text1: true });
+                else if (entry.isIntersecting && entry.target.id === 'text2') setAnimation({ ...animation, text2: true });
+                else if (entry.isIntersecting && entry.target.id === 'text3') setAnimation({ ...animation, text3: true });
+                else if (entry.isIntersecting && entry.target.id === 'text4') setAnimation({ ...animation, text4: true });
+                else if (entry.isIntersecting && entry.target.id === 'text5') setAnimation({ ...animation, text5: true });
+                else if (entry.isIntersecting && entry.target.id === 'text6') setAnimation({ ...animation, text6: true });
+            });
+        });
 
-		const textP1 = document.getElementById("text1");
-		const img1 = document.getElementById("img1");
+        const text0 = document.getElementById('text0');
+        const text1 = document.getElementById('text1');
+        const text2 = document.getElementById('text2');
+        const text3 = document.getElementById('text3');
+        const text4 = document.getElementById('text4');
+        const text5 = document.getElementById('text5');
+        const text6 = document.getElementById('text6');
 
-		if (textP1) observer.observe(textP1);
-		else console.error("text 1 nenalezen observerem!");
+        if (text0) {
+            observer.observe(text0);
+        } else console.error('text0 nenalezen observerem! Coponent-About');
 
-		if (img1) observer.observe(img1);
-		else console.error("img 1 nenalezen observerem!");
-	}, []);
+        if (text1) {
+            observer.observe(text1);
+        } else console.error('text1 nenalezen observerem! Coponent-About');
 
-	// if (loading) return <h1>Loading...</h1>;
+        if (text2) observer.observe(text2);
+        else console.error('text2 nenalezen observerem! Coponent-About');
 
-	return (
-		<section className="about">
-			{/* <img className="wave" src={light} alt="" /> */}
-			<div className="first-block">
-				<div className="box">
-					<div className="nadpis">
-						<h1>O mně</h1>
-						<p className="text1">Jmenuji se Jiří Strnadel</p>
-						<p className="text2">a věnuji se vývoji webových aplikací se zaměřením na React a TypeScript.</p>
-						<div className="icons-box">
+        if (text3) observer.observe(text3);
+        else console.error('text3 nenalezen observerem! Coponent-About');
 
-						<StackIcon className="react-icon react" name={"reactjs"} />
-						<StackIcon className="react-icon typescript" name={"typescript"} />
-						<StackIcon className="react-icon csharp" name={"csharp"} />
-						</div>
-					</div>
-					<img className="me" src={imgBaldMan} alt="" />
-				</div>
-			</div>
+        if (text4) observer.observe(text4);
+        else console.error('text4 nenalezen observerem! Coponent-About');
 
-			<div className="second-block">
-				<div className="box">
-					<img className={animationImg1 ? "working active" : "working"} id="img1" src={comp} alt="" />
-					<p className={animationText1 ? "active" : ""} id="text1">
-						Programování mě nadchlo natolik, že mu věnuji každý volný moment už více než rok a půl. Neustále se učím
-						nové technologie a zdokonaluji své dovednosti.
-					</p>
-				</div>
-			</div>
+        if (text5) observer.observe(text5);
+        else console.error('text3 nenalezen observerem! Coponent-About');
 
-			<div className="third-block">
-				<Slider />
-			</div>
+        if (text6) observer.observe(text6);
+        else console.error('text4 nenalezen observerem! Coponent-About');
+    }, []);
 
-			<div className="fourth-block">
-				<h2>Zkušenosti</h2>
-				<p>
-					Mám zkušenosti s JavaScriptem, SCSS, HTML, CSS, Git a databázemi jako Firebase. Zároveň si rozšiřuji znalosti
-					v C#, kde se pohybuji na mírně pokročilé úrovni.
-				</p>
-				<TechBars />
-			</div>
+    // if (loading) return <h1>Loading...</h1>;
 
-			<div className="fifth-block">
-				<p>
-					Nyní pracuji jako elektro údržbář a programátor průmyslových robotů Kuka, Reis, Yaskawa a PLC Simatic. Tato
-					práce mě naučila analytickému myšlení a efektivnímu řešení problémů, což dnes využívám při vývoji aplikací.
-				</p>
-				<div className="robot-div">
-					<img className="robot" src={robot} alt="" />
-				</div>
-			</div>
+    return (
+        <section className="about">
+            <div className="first-block">
+                <div className="box">
+                    <div className="nadpis">
+                        <h1 className={animation.text0 ? 'active t0' : 't0'}>O mně</h1>
+                        <p className={animation.text0 ? 't1 active' : 't1'}>Jmenuji se Jiří Strnadel</p>
+                        <p className={animation.text0 ? 't2 active' : 't2'} id="text0">
+                            a věnuji se vývoji webových aplikací se zaměřením na React a TypeScript.
+                        </p>
+                        <div className={animation.text0 ? 'icons-box active' : 'icons-box'}>
+                            <StackIcon className="react-icon react" name={'reactjs'} />
+                            <StackIcon className="react-icon typescript" name={'typescript'} />
+                            <StackIcon className="react-icon csharp" name={'csharp'} />
+                        </div>
+                    </div>
+                    <img className="me" src={imgBaldMan} alt="" />
+                </div>
+            </div>
 
-			<div className="sixth-block">
-				<p>
-					Hledám příležitost, kde bych mohl své dovednosti dále rozvíjet a přispět svým nadšením pro moderní frontendový
-					vývoj.
-				</p>
+            <div className="second-block">
+                <div className="box">
+                    <img className={animation.text1 ? 'working active' : 'working'} id="text1" src={comp} alt="" />
+                    <p className={animation.text2 ? 'active' : ''} id="text2">
+                        Programování mě nadchlo natolik, že mu věnuji každý volný moment už více než rok a půl. Neustále se učím nové
+                        technologie a zdokonaluji své dovednosti.
+                    </p>
+                </div>
+            </div>
 
-				<Email />
-			</div>
-		</section>
-	);
+            <div className="third-block">
+                <Slider />
+            </div>
+
+            <div className="fourth-block">
+                <h2 className={animation.text3 ? 'active' : ''} id="text3">
+                    Zkušenosti
+                </h2>
+                <p className={animation.text4 ? 'active' : ''} id="text4">
+                    Mám zkušenosti s JavaScriptem, SCSS, HTML, CSS, Git a databázemi jako Firebase. Zároveň si rozšiřuji znalosti v C#, kde
+                    se pohybuji na mírně pokročilé úrovni.
+                </p>
+                <TechBars />
+            </div>
+
+            <div className="fifth-block">
+                <p className={animation.text5 ? 'active' : ''} id="text5">
+                    Nyní pracuji jako elektro údržbář a programátor průmyslových robotů Kuka, Reis, Yaskawa a PLC Simatic. Tato práce mě
+                    naučila analytickému myšlení a efektivnímu řešení problémů, což dnes využívám při vývoji aplikací.
+                </p>
+                <div className="robot-div">
+                    <img className="robot" src={robot} alt="" />
+                </div>
+            </div>
+
+            <div className="sixth-block">
+                <p className={animation.text6 ? 'text6 active' : 'text6'} id="text6">
+                    Hledám příležitost, kde bych mohl své dovednosti dále rozvíjet a přispět svým nadšením pro moderní frontendový vývoj.
+                </p>
+
+                <Email />
+            </div>
+        </section>
+    );
 };
 
 export default About;
